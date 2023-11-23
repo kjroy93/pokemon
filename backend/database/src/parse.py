8e49734cdc36c166acba49f66d751c143a917bef
kjroy93 <kmarrero1993@gmail.com>
2023-11-23 22:02:55 +0100

    level up atk and TM remain. Mega_Pokemon class to be builded, as the only form exception that need special treatment

--- backend/database/src/parse.py
+++ backend/database/src/parse.py
@@ -1,9 +1,22 @@
 # Special Functions in order for main class more legible
 
-from bs4 import Tag
+from bs4 import Tag,ResultSet
+
+def elemental_types(location:Tag) -> list:
+    types = []
+    for tag in location:
+        a_tag = tag.find('img')
+        if a_tag:
+            type_text = a_tag['alt']
+            types.append(type_text)
+
+    s = 'Attacking Move Type: ','-type'
+    for string in s:
+        types = list(map(lambda x: x.replace(string,''),types))
+    
+    return types
 
 def get_parents(parents: list) -> dict:
-    # Crear un diccionario vacío para almacenar los grupos de Pokémon
     pokemon_groups = {}
     current_group = parents[0]
     pokemon_groups[current_group] = []
@@ -21,7 +34,6 @@ def process_hidden_ability(tag:Tag, abilities_dict:dict, form_abilities_dict:dic
         to_join = [ability, ability_text]
         result = ''.join(to_join)
 
-        # Verificar si se están procesando habilidades de Form
         if form_abilities_flag:
             form_abilities_dict['hidden_ability'].append(result)
         else:
@@ -51,3 +63,28 @@ def process_form_ability(tag:Tag, form_abilities_flag:bool):
     if "Form" in father:
         form_abilities_flag = True
     return form_abilities_flag
+
+def find_table_by_class(gen:int, main_table:ResultSet, class_name: str, index: int) -> ResultSet:
+    if gen != 8:
+        return main_table[index].find_all('td', {'class': class_name})
+    else:
+        return main_table[index + 1].find_all('td', {'class': class_name})
+
+def stats_calculation(stat:str, base:int, EV:int, level:int=50, nature=1, IV=31):
+    if stat == 'hp':
+        hp = ((IV+2*base+(EV/4))*level/100)+10+level
+        return hp
+    else:
+        st =  (((IV + 2 * base + (EV/4) ) * level/100 ) + 5) * nature
+        return st
+
+def detect_new_forms(main_table:ResultSet):
+    # Buscar todas las tablas en el primer div
+    tables = main_table[0].find_all('table', {'class': 'dextable'})
+
+    # Buscar la posición exacta de la línea con <td class="fooevo" colspan="6">
+    for i, table in enumerate(tables):
+        if table.find('td', {'class': 'fooevo', 'colspan': '6'}):
+            break
+    
+    return i
--- backend/database/src/src.py
+++ backend/database/src/src.py
@@ -8,7 +8,7 @@ import requests
 import re
 from bs4 import BeautifulSoup,ResultSet
 
-from .parse import get_parents,process_ability,process_form_ability,process_hidden_ability
+from .parse import get_parents,process_ability,process_form_ability,process_hidden_ability,find_table_by_class,elemental_types,stats_calculation
 
 URL = "https://www.serebii.net/index2.shtml"
 
@@ -53,31 +53,36 @@ class Pokemon():
         if gen != 8:
             url = f'https://www.serebii.net/{chain}/{str(number_name).zfill(3)}.shtml'
         else:
-            f'https://www.serebii.net/{chain}/{number_name}/'
+            url = f'https://www.serebii.net/{chain}/{number_name}/'
         
         self.html = requests.get(url)
         self.soup = BeautifulSoup(self.html.text, 'html.parser')
         self.number = '#{}'.format(str(number_name).zfill(3))
         self.gen = gen
 
-    def __basic_tables(self) -> ResultSet:
+    def __basic_tables(self, type_of_table: str) -> ResultSet:
         all_divs = self.soup.find_all('div', attrs={'align': 'center'})
 
-        if self.gen != 8:
-            foo_info = all_divs[0].find_all('td', {'class': 'fooinfo'})
-        else:
-            foo_info = all_divs[1].find_all('td', {'class': 'fooinfo'})
-
-        return foo_info
-    
+        match type_of_table:
+            case 'fooinfo':
+                return find_table_by_class(self.gen,all_divs,'fooinfo',0)
+            
+            case 'footype':
+                return find_table_by_class(self.gen,all_divs,'footype',0)
+            
+            case 'bases':
+                return all_divs
+   
     def name(self):
-        location = self.__basic_tables()[1]
+        table = self.__basic_tables('fooinfo')
+        location = table[1]
         self._name = location.text
     
     def gender(self):
         self._gender_info = []
         strings_to_search = 'Genderless','Unknown'
-        location = self.__basic_tables()[4]
+        table = self.__basic_tables('fooinfo')
+        location = table[4]
 
         list_to_search = location.text.split()
 
@@ -95,28 +100,29 @@ class Pokemon():
             self._gender_info.append(gender_info_entry)
 
     def height(self):
-        location = self.__basic_tables()[6]
+        table = self.__basic_tables('fooinfo')
+        location = table[6]
         height = location.br.next_sibling.text
         self._height = height.split('\r\n\t\t\t').pop(1)
 
     def weight(self):
-        location = self.__basic_tables()[7]
+        location = self.__basic_tables('fooinfo')[7]
         weight = location.br.next_sibling.text
         self._weight = weight.split('\r\n\t\t\t').pop(1)
 
     def capture_rate(self):
-        location = self.__basic_tables()[8]
+        location = self.__basic_tables('fooinfo')[8]
         self._capture_rate = int(location.text)
     
     def breeding_steps(self):
-        location = self.__basic_tables()[9]
+        location = self.__basic_tables('fooinfo')[9]
         self._egg_steps = int(location.text.replace(',',''))
 
     def abilities(self):
         self._abilities = {'ability': [], 'hidden_ability': []}
         self._form_abilities = {'ability': [], 'hidden_ability': []}
 
-        location = self.__basic_tables()[10]
+        location = self.__basic_tables('fooinfo')[10]
         tags = location.find_all('b')
 
         skip_next_flag = False
@@ -133,17 +139,52 @@ class Pokemon():
                 form_abilities_flag = process_form_ability(tag, form_abilities_flag)
                 process_ability(tag, self._abilities, self._form_abilities, form_abilities_flag)
 
-    def __process_form(self, location, form_name):
+    def __process_formulary(self, location, form_name):
         box = location.find('form', {'name': form_name}).find_all('option')
         parents = [p.text for p in box]
         return get_parents(parents)
 
     def egg_group(self):
-        location = self.__basic_tables()[15]
+        location = self.__basic_tables('fooinfo')[15]
         quantity = len(location.find_all('form'))
 
         if quantity > 1:
-            self._parents_0 = self.__process_form(location,'breed')
-            self._parents_1 = self.__process_form(location,'breed2')
+            self._parents_0 = self.__process_formulary(location,'breed')
+            self._parents_1 = self.__process_formulary(location,'breed2')
         else:
-            self._parents_0 = self.__process_form(location,'breed')
+            self._parents_0 = self.__process_formulary(location,'breed')
+    
+    def weakness(self):
+        effectiveness = []
+
+        location = self.__basic_tables('footype')
+
+        elements = elemental_types(location)
+
+        filtered = [tag for tag in location if '*' in tag.get_text(strip=True)]
+        filtered_t = [i.text for i in filtered]
+
+        val = list(map(lambda x: x.replace('*',''),filtered_t))
+
+        for i in val:
+            try:
+                effectiveness.append(int(i))
+            except ValueError:
+                effectiveness.append(float(i))
+
+        self._weakness = dict(zip(elements,effectiveness))
+    
+    def stats(self):
+        location = self.__basic_tables('bases')
+        bases = location[0].find('td', string=re.compile("Base Stats - Total.*")).find_next_siblings('td')
+
+        self._hp = int(bases[0].text)
+        self._atk = int(bases[1].text)
+        self._def = int(bases[2].text)
+        self._sp_atk = int(bases[3].text)
+        self._sp_def = int(bases[4].text)
+        self._spd = int(bases[5].text)
+
+class Mega_Pokemon():
+    def __init__(self):
+        self.pokemon = Pokemon()
