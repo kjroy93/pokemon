7cc24d1877bf57f594ea52a58cc55843a76e8da4
kjroy93 <kmarrero1993@gmail.com>
2023-11-27 23:22:27 +0100

    previous step for TM and Level Up attacks

--- backend/database/src/parse.py
+++ backend/database/src/parse.py
@@ -1,22 +1,72 @@
 # Special Functions in order for main class more legible
 
+from typing import Literal
 from bs4 import Tag,ResultSet
 
-def elemental_types(location:Tag) -> list:
-    types = []
-    for tag in location:
-        a_tag = tag.find('img')
-        if a_tag:
-            type_text = a_tag['alt']
-            types.append(type_text)
+def find_table_by_class(gen:int, main_table:ResultSet, class_name:str, normal_form:str=None, index:int=0) -> ResultSet:
+    """
+    Function that goes to the exact class that contains the information, with the information that recives from self.__basic_tables() method in Pokémon class:
 
-    s = 'Attacking Move Type: ','-type'
-    for string in s:
-        types = list(map(lambda x: x.replace(string,''),types))
+    Requirements:
+
+    - gen: Generation.
+    - main_table: This is BeautifulSoup HTML.text parser that contains all the information.
+    - class_name: The class that needs to be located in the HTML.
+    - normal_form: WARNING! This only applies if the Pokémon contains a Mega Evolution. This value comes preloaded from the Mega Pokemon class.\n
+    The flag cames with None from default, so it does not need change.
+    - index: Proper location of the table. Do not give any value, unless you know what you are doing, in the main center distribution of the webpage.
     
-    return types
+    """
+    match normal_form:
+        case None:
+            if gen != 8:
+                return main_table[index].find_all('td', {'class': class_name})
+            else:
+                return main_table[index + 1].find_all('td', {'class': class_name})
+            
+        case 'form':
+            if gen != 8:
+                return main_table[index].find_all('table', {'class': 'dextable'})
 
-def get_parents(parents: list) -> dict:
+def get_elemental_types(location:Tag, info:Literal['weakness'], form:str=None, elements:list=None) -> list:
+    match form:
+        case None:
+            types = []
+            if info == 'weakness':
+                location = location[0:18]
+
+            for tag in location:
+                a_tag = tag.find('img')
+                if a_tag and not isinstance(a_tag,Tag):
+                    continue
+                else:
+                    print(f'aqui se busca el tipo {a_tag}')
+                    type_text = a_tag['alt']
+                    types.append(type_text)
+
+            s = 'Attacking Move Type: ','-type'
+            for string in s:
+                types = list(map(lambda x: x.replace(string,''),types))
+            
+            return types
+        
+        case form:
+            types = []
+            for tag in location:
+                a_tag = tag.find('img')
+                if a_tag and not isinstance(a_tag,Tag):
+                    continue
+                else:
+                    type_text = a_tag['src']
+
+                for element in elements:
+                    ty = element.lower()
+                    if ty in a_tag:
+                        types.append(element)
+            
+            return types
+
+def get_parents(parents:list) -> dict:
     pokemon_groups = {}
     current_group = parents[0]
     pokemon_groups[current_group] = []
@@ -26,7 +76,7 @@ def get_parents(parents: list) -> dict:
     
     return pokemon_groups
 
-def process_hidden_ability(tag:Tag, abilities_dict:dict, form_abilities_dict:dict, skip_next_flag:bool, form_abilities_flag:bool):
+def process_hidden_ability(tag:Tag, abilities_dict:dict, form_abilities_dict:dict, skip_next_flag:bool, form_abilities_flag:bool) -> bool:
     next_tag = tag.find_next('b')
     if next_tag:
         ability = next_tag.text
@@ -60,31 +110,25 @@ def process_ability(tag:Tag, abilities_dict:dict, form_abilities_dict:dict, form
 
 def process_form_ability(tag:Tag, form_abilities_flag:bool):
     father = tag.text
-    if "Form" in father:
+    if 'Form' in father:
         form_abilities_flag = True
     return form_abilities_flag
 
-def find_table_by_class(gen:int, main_table:ResultSet, class_name: str, index: int) -> ResultSet:
-    if gen != 8:
-        return main_table[index].find_all('td', {'class': class_name})
-    else:
-        return main_table[index + 1].find_all('td', {'class': class_name})
-
-def stats_calculation(stat:str, base:int, EV:int, level:int=50, nature=1, IV=31):
+def stats_calculation(stat:str, base:int, EV:int=0, level:int=50, nature=1, IV=31):
     if stat == 'hp':
         hp = ((IV+2*base+(EV/4))*level/100)+10+level
         return hp
     else:
-        st =  (((IV + 2 * base + (EV/4) ) * level/100 ) + 5) * nature
+        st = (((IV + 2 * base + (EV/4) ) * level/100 ) + 5) * nature
         return st
 
-def detect_new_forms(main_table:ResultSet):
-    # Buscar todas las tablas en el primer div
+def detect_new_forms(pokemon_name:str, main_table:ResultSet) -> (int|str):
+    #Get al tables from the main center table
     tables = main_table[0].find_all('table', {'class': 'dextable'})
 
-    # Buscar la posición exacta de la línea con <td class="fooevo" colspan="6">
+    #Search for the line that contains <td class="fooevo" colspan="6">
     for i, table in enumerate(tables):
         if table.find('td', {'class': 'fooevo', 'colspan': '6'}):
-            break
+            return i
     
-    return i
+    return f'{pokemon_name} does not have Mega'
--- backend/database/src/src.py
+++ backend/database/src/src.py
@@ -1,20 +1,21 @@
-# Standard libraries of Python
+from typing import Tuple
 
-from functools import wraps
-from typing import Literal,Callable
-
-# Dependencies
+#Dependencies
 import requests
 import re
-from bs4 import BeautifulSoup,ResultSet
+from bs4 import BeautifulSoup,ResultSet,Tag
 
-from .parse import get_parents,process_ability,process_form_ability,process_hidden_ability,find_table_by_class,elemental_types,stats_calculation
+from backend.database.src import parse
 
 URL = "https://www.serebii.net/index2.shtml"
 
 assert requests.get(URL).status_code == 200, 'There is a problem with Serebii webpage'
 
 def gen_url(gen: int):
+    """
+    It defines an aspect of the URL of the webpage.
+    The gen attribute goes from 1 to 8. It is mandatory, in order for the program to work. You use it in the beginning of Pokémon class."
+    """
     gens = {
         1: 'pokedex',
         2: 'pokedex-gs',
@@ -29,19 +30,25 @@ def gen_url(gen: int):
     return gens[gen]
 
 class Pokemon():
-    def __init__(self, gen:int, number_name):
+    def __init__(self, gen:int, number_name:Tuple[int|str]):
         """
-        Clase que representa un Pokémon.
+        Class that represents a Pokémon.
 
         Parameters:
-        - gen (int): Generación del Pokémon.
-        - number_name (int/str): Número del Pokémon (para gen < 8) o nombre del Pokémon (para gen >= 8).
+        - gen (int): The game data you want to see. Please consider, that you must introduce a number, or is not gonna work.
+        - number_name (int/str): Pokémon number (for gen < 8) or Pokémon name (for gen >= 8).
 
-        Attributes:
-        - html: Contiene el HTML de la página del Pokémon.
-        - soup: BeautifulSoup object para analizar la página HTML.
-        - number: Número del Pokémon.
-        - gen: Generación del Pokémon.
+        Basic Attributes:
+        - html: It contains the HTML of the webpage we are going to scrap This is done with requests library.
+        - soup: BeautifulSoup object that contains the HTML text from the webpage.
+        - p_number: Pokémon number.
+        - gen: Generation.
+
+        Every attribute that contains 'p_', represents the information of the Pókemon you scrapted.
+
+        You can use any of the methods provided in this class. There is no mandatory execution order. For futher information,
+        please refer to them.
+
         """
 
         if gen < 8 and not isinstance(number_name, int):
@@ -57,29 +64,62 @@ class Pokemon():
         
         self.html = requests.get(url)
         self.soup = BeautifulSoup(self.html.text, 'html.parser')
-        self.number = '#{}'.format(str(number_name).zfill(3))
+        self.p_number = '#{}'.format(str(number_name).zfill(3))
         self.gen = gen
 
     def __basic_tables(self, type_of_table: str) -> ResultSet:
+        """
+        Private method that specifies the classes to searcth in the HTML.text soup:
+
+        Attribute:
+        
+        - type_of_table: it contains the string that specifies the class to be located.
+        
+        """
         all_divs = self.soup.find_all('div', attrs={'align': 'center'})
 
         match type_of_table:
             case 'fooinfo':
-                return find_table_by_class(self.gen,all_divs,'fooinfo',0)
+                return parse.find_table_by_class(self.gen,all_divs,'fooinfo')
             
             case 'footype':
-                return find_table_by_class(self.gen,all_divs,'footype',0)
+                return parse.find_table_by_class(self.gen,all_divs,'footype')
             
             case 'bases':
                 return all_divs
+            
+            case 'elements':
+                return parse.find_table_by_class(self.gen,all_divs,'cen')
    
     def name(self):
+        """
+        Method to obtain the name of the Pokémon:
+        
+        Attribute:
+
+        - p_name: Pokémon name
+        """
         table = self.__basic_tables('fooinfo')
         location = table[1]
-        self._name = location.text
+        self.p_name = location.text
     
     def gender(self):
-        self._gender_info = []
+        """
+        It defines the % of the Pokémon Gender. This matters specially if you want to breed Pokémon an inherit Hidden Ability more easilly:
+
+        Female has a 60% chance of pass her Hidden Ability. It also, allways pass the Pokeball where she was captured.\n
+
+        Males pass Hidden Ability if the other parent is a Ditto. It also applies to his Pokeball. Males also pass their movements.
+        This means, that if the species of the mother learns that move inherited via egg, the father pass that move.\n
+
+        Genderless Pokémon can pass their Hidden Ability and Pokeball with a Ditto parent.
+
+        Please note that if the parents are from the same species, the Pokeball has 50% chance of inherith any of them.\n
+
+        - p_gender_info: gender probability
+
+        """
+        self.p_gender_info = []
         strings_to_search = 'Genderless','Unknown'
         table = self.__basic_tables('fooinfo')
         location = table[4]
@@ -87,42 +127,70 @@ class Pokemon():
         list_to_search = location.text.split()
 
         if strings_to_search[0] in list_to_search:
-            self._gender_info = strings_to_search[0]
+            self.p_gender_info = strings_to_search[0]
         elif strings_to_search[1] in list_to_search:
-            self._gender_info = strings_to_search[1]
+            self.p_gender_info = strings_to_search[1]
 
         genders = location.select('td.fooinfo font')
 
         for gender in genders:
             sym = gender.text
             percentage = gender.find_next('td').text.strip()
-            gender_info_entry = f"{sym}: {percentage}"
-            self._gender_info.append(gender_info_entry)
+            gender_info_entry = f'{sym}: {percentage}'
+            self.p_gender_info.append(gender_info_entry)
+    
+    def elements(self):
+        """
+
+        It determines the elemental types of the Pokémon. This is basic in order to know weakness and STAB (explained later in Damage Calculation).
+
+        - p_elements: Elemental Type of Pokemon.
+
+        """
+        table = self.__basic_tables('elements')
+        location = table[0]
+
+        self.p_elements = parse.get_elemental_types(location)
 
     def height(self):
+        """
+        It defines the height of the Pokémon. Just fun fact.
+
+        -p_height: height of Pokémon
+        """
         table = self.__basic_tables('fooinfo')
         location = table[6]
         height = location.br.next_sibling.text
-        self._height = height.split('\r\n\t\t\t').pop(1)
+        self.p_height = height.split('\r\n\t\t\t').pop(1)
 
     def weight(self):
-        location = self.__basic_tables('fooinfo')[7]
+        """
+        It defines the weight of the Pokémon. This matters because of some moves that works with this Attribute.
+
+        - p_weight: weight of Pokémon.
+        """
+        table = self.__basic_tables('fooinfo')
+        location = table[7]
         weight = location.br.next_sibling.text
-        self._weight = weight.split('\r\n\t\t\t').pop(1)
+        self.p_weight = weight.split('\r\n\t\t\t').pop(1)
 
     def capture_rate(self):
-        location = self.__basic_tables('fooinfo')[8]
-        self._capture_rate = int(location.text)
+        table = self.__basic_tables('fooinfo')
+        location = table[8]
+        self.p_capture_rate = int(location.text)
     
     def breeding_steps(self):
-        location = self.__basic_tables('fooinfo')[9]
-        self._egg_steps = int(location.text.replace(',',''))
+        table = self.__basic_tables('fooinfo')
+        location = table[9]
+        self.p_egg_steps = int(location.text.replace(',',''))
 
     def abilities(self):
-        self._abilities = {'ability': [], 'hidden_ability': []}
-        self._form_abilities = {'ability': [], 'hidden_ability': []}
+        self.p_abilities = {'ability': [], 'hidden_ability': []}
+        self.p_form_abilities = {'ability': [], 'hidden_ability': []}
 
-        location = self.__basic_tables('fooinfo')[10]
+        table = self.__basic_tables('fooinfo')
+        location = table[10]
+
         tags = location.find_all('b')
 
         skip_next_flag = False
@@ -134,37 +202,37 @@ class Pokemon():
                 continue
 
             if "Hidden Ability" in tag.text:
-                skip_next_flag = process_hidden_ability(tag, self._abilities, self._form_abilities, skip_next_flag, form_abilities_flag)
+                skip_next_flag = parse.process_hidden_ability(tag,self.p_abilities,self.p_form_abilities,skip_next_flag,form_abilities_flag)
             else:
-                form_abilities_flag = process_form_ability(tag, form_abilities_flag)
-                process_ability(tag, self._abilities, self._form_abilities, form_abilities_flag)
+                form_abilities_flag = parse.process_form_ability(tag,form_abilities_flag)
+                parse.process_ability(tag,self.p_abilities,self.p_form_abilities,form_abilities_flag)
 
-    def __process_formulary(self, location, form_name):
+    def __process_formulary(self, location:Tag, form_name:str):
         box = location.find('form', {'name': form_name}).find_all('option')
         parents = [p.text for p in box]
-        return get_parents(parents)
+        return parse.get_parents(parents)
 
     def egg_group(self):
-        location = self.__basic_tables('fooinfo')[15]
+        table = self.__basic_tables('fooinfo')
+        location = table[15]
         quantity = len(location.find_all('form'))
 
         if quantity > 1:
-            self._parents_0 = self.__process_formulary(location,'breed')
-            self._parents_1 = self.__process_formulary(location,'breed2')
+            self.p_parents_0 = self.__process_formulary(location,'breed')
+            self.p_parents_1 = self.__process_formulary(location,'breed2')
         else:
-            self._parents_0 = self.__process_formulary(location,'breed')
+            self.p_parents_0 = self.__process_formulary(location,'breed')
     
     def weakness(self):
         effectiveness = []
 
         location = self.__basic_tables('footype')
 
-        elements = elemental_types(location)
+        self.elemental_types = parse.get_elemental_types(location,'weakness')
 
-        filtered = [tag for tag in location if '*' in tag.get_text(strip=True)]
-        filtered_t = [i.text for i in filtered]
+        filtered = [tag.text for tag in location[18:36] if '*' in tag.get_text(strip=True)]
 
-        val = list(map(lambda x: x.replace('*',''),filtered_t))
+        val = list(map(lambda x: x.replace('*',''),filtered))
 
         for i in val:
             try:
@@ -172,19 +240,98 @@ class Pokemon():
             except ValueError:
                 effectiveness.append(float(i))
 
-        self._weakness = dict(zip(elements,effectiveness))
+        self.p_weakness = dict(zip(self.elemental_types,effectiveness))
     
     def stats(self):
         location = self.__basic_tables('bases')
         bases = location[0].find('td', string=re.compile("Base Stats - Total.*")).find_next_siblings('td')
 
-        self._hp = int(bases[0].text)
-        self._atk = int(bases[1].text)
-        self._def = int(bases[2].text)
-        self._sp_atk = int(bases[3].text)
-        self._sp_def = int(bases[4].text)
-        self._spd = int(bases[5].text)
+        self.p_hp = int(bases[0].text)
+        self.p_atk = int(bases[1].text)
+        self.p_def = int(bases[2].text)
+        self.p_sp_atk = int(bases[3].text)
+        self.p_sp_def = int(bases[4].text)
+        self.p_spd = int(bases[5].text)
 
 class Mega_Pokemon():
-    def __init__(self):
-        self.pokemon = Pokemon()
+    def __init__(self, pokemon: Pokemon):
+        self.pokemon = pokemon
+    
+    def __basic_tables(self, type_of_table:str, form:str='form') -> ResultSet:
+        """
+        Private method that specifies the classes to searcth in the HTML.text soup:
+
+        Attribute:
+        
+        type_of_table: it contains the string that specifies the class to be located.
+
+        The main difference with Pokemon class, is that the Attribute form comes by default, so it does not need to change.
+        
+        """
+        all_divs = self.pokemon.soup.find_all('div', attrs={'align': 'center'})
+
+        match type_of_table:
+            case type_of_table:
+                table = parse.find_table_by_class(self.pokemon.gen,all_divs,'fooinfo',form)
+                n_f_position = parse.detect_new_forms(self.pokemon.p_name,table)
+
+                if isinstance(n_f_position,str):
+                    raise ValueError(n_f_position)
+                
+                return table, n_f_position
+    
+    def name(self):
+        try:
+            table,position = self.__basic_tables('fooinfo')
+            location = table[position+1].find('td', class_='fooinfo')
+            self.m_name = location.text
+        except ValueError as e:
+            print(f'Error: {e}')
+    
+    def elements(self):
+        try:
+            table,position = self.__basic_tables('fooinfo')
+            location = table[position+1].find('td', class_='cen')
+            self.m_elements = parse.get_elemental_types(location,None,'yes',self.pokemon.elemental_types)
+        except ValueError as e:
+            print(f'Error: {e}')
+    
+    def weakness(self):
+        m_weak = []
+
+        if self.m_elements == self.pokemon.p_elements:
+            pass
+
+        else:
+            try:
+                table,position = self.__basic_tables('fooinfo')
+                location = table[position+2]
+                filtered = list(filter(lambda x: '*' in x.text, location.find_all('td', {'class': 'footype'})))
+                filtered = [tag.text for tag in filtered if '*' in tag.get_text(strip=True)]
+
+                val = list(map(lambda x: x.replace('*',''),filtered))
+
+                for i in val:
+                    try:
+                        m_weak.append(int(i))
+                    except ValueError:
+                        m_weak.append(float(i))
+                
+                self.m_weakness = dict(zip(self.pokemon.elemental_types,m_weak))
+            except ValueError as e:
+                print(f'Error : {e}')
+    
+    def m_base(self):
+        try:
+            table,position = self.__basic_tables('base')
+
+            bases = table[position+4].find('td', string=re.compile("Base Stats - Total.*")).find_next_siblings('td')
+            
+            self.m_hp = int(bases[0].text)
+            self.m_atk = int(bases[1].text)
+            self.m_def = int(bases[2].text)
+            self.m_sp_atk = int(bases[3].text)
+            self.m_sp_def = int(bases[4].text)
+            self.m_spd = int(bases[5].text)  
+        except ValueError as e:
+            print(f'Error: {e}')
